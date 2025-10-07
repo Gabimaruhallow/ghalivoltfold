c0bb22caf150918d9055c831c4746d0e7e8ac876
Gabimaruhallow <yeyemalak2@gmail.com>
2025-10-07 18:28:57 +0200

    Delete workspace-3c19bbbe-9f35-4a34-80f5-89031e27864f (1)~ directory

--- workspace-3c19bbbe-9f35-4a34-80f5-89031e27864f (1)~/src/app/src/app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{fontFamily:"sans-serif", textAlign:"center", marginTop:"50px"}}>
-      <h1>Hello VoltFold ⚡</h1>
-      <p>Your Next.js app is deployed successfully!</p>
-    </div>
-  )
-}
